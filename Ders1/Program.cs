c986c4fb0bfcaa551fd56eabb63098d7b1b8ffb9
dedasame <ecekarakoc36@gmail.com>
2023-11-26 17:13:01 +0300

    Proje dosyası ekle.

---
+++ Ders1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //QWEQWEQWE
+
+            Console.WriteLine("WELCOME PLAYER"); //Yazar ve alt satira gecer
+            Console.Write("BYE!");//Yazar alt satira gecme yok!!
+            Console.ReadLine();  //Konsolun kaybolmasini onler.
+
+
+
+
+
+        }
+    }
+}
