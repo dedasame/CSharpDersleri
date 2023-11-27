09f3da461bf2568807596e0aa6996871f038e2c4
dedasame <ecekarakoc36@gmail.com>
2023-11-27 20:31:59 +0300

    metot

---
+++ Metot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            hello();
+
+
+            Console.WriteLine(plus(5, 7));
+
+            int a = plus(20, 10);
+
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+
+        static void hello()
+        {
+            Console.WriteLine("HELLO");
+        
+        }
+
+        static int plus(int a, int b)
+        {
+            return a + b;
+
+        }
+
+
+
+
+
+
+
+    }
+}
