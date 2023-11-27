ba3d8bde160e961876e3b4dedffb570126e1d4cd
dedasame <ecekarakoc36@gmail.com>
2023-11-27 20:24:01 +0300

    ikincigun

--- Ders1/Program.cs
+++ Ders1/Program.cs
@@ -15,11 +15,31 @@ namespace Ders1
 
             Console.WriteLine("WELCOME PLAYER"); //Yazar ve alt satira gecer
             Console.Write("BYE!");//Yazar alt satira gecme yok!!
-            Console.ReadLine();  //Konsolun kaybolmasini onler.
+            Console.ReadLine();  //Konsolun kaybolmasini onler. Kullanicidan veri alir.
 
+            //Tur Donusumu 
 
+            string age = Console.ReadLine(); //kullanicidan her zaman string alinir.
 
+            int a = Convert.ToInt32(age);
 
+            int b = int.Parse(age);
+
+            Console.WriteLine("Toplam = " + (a + b)); //Karisiklik olmasin diye bir parantez daha ekliyoruz.
+
+            // break; continue;
+
+            //arrays
+
+            int[] sayilar = new int[4]; // sayilar[i] = Convert.ToInt32(Console.ReadLine());
+            string[] isimler = { "Name", "Name2", "Name3" };
+
+            //for each dongusu
+
+            foreach (string name in isimler)
+            {
+                Console.WriteLine(name);
+            }
 
         }
     }
